943b0ee1b8a46b04b4313339dae4d62b95623dc5
Sviatoslav Zapolskyi <sviatoslav.zapolskyi@opower.com>
2018-06-04 16:35:04 +0300

    small fixes

--- test/find_freelancers.rb
+++ test/find_freelancers.rb
@@ -3,14 +3,14 @@ require 'minitest/autorun'
 
 module Upwork
   module Matchers
-    def key_include(expected, actual)
+    def key_include(actual, expected)
       actual.select do |_, val|
         val = val.join if val.kind_of?(Array)
         val.downcase.include? expected.downcase
       end.any?
     end
 
-    def match_profile(expected, actual)
+    def match_profile(actual, expected)
       actual.select do |key, val|
         if val.kind_of?(Array)
           (expected[key] - val).any?
@@ -22,6 +22,8 @@ module Upwork
   end
 
   class FindFreelancersTest < Minitest::Unit::TestCase
+
+    include Upwork::Matchers
 
     KEYWORD = 'ruby'
 
@@ -42,22 +44,20 @@ module Upwork
 
       freelancer_profile_page = search_results_page.openSampleProfile
       freelancer = freelancer_profile_page.data
-      expect(freelancer).to match_profile(freelancers.find { |f| f[:name] == freelancer[:name] })
-
-      assert match_profile(freelancer_profile_page.data,)
+      assert match_profile(freelancer, freelancers.find { |f| f[:name] == freelancer[:name] })
       assert key_include(freelancer_profile_page.data, KEYWORD)
     end
 
-    #
-    # def test_change_search_contexts
-    #   ['Find Jobs', 'Find Freelancers', 'Find Jobs'].each do |search_context|
-    #     page.search_context = search_context
-    #     assert page.search_context.eql?(search_context)
-    #   end
-    # end
+
+    def test_change_search_contexts
+      ['Find Jobs', 'Find Freelancers', 'Find Jobs'].each do |search_context|
+        page.search_context = search_context
+        assert page.search_context.eql?(search_context)
+      end
+    end
 
     def teardown
-      UpWork::Driver.driver.quit
+      # UpWork::Driver.driver.quit
     end
   end
 end
